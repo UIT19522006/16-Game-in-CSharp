ac2769d08d618cebfb06d9c0622c724fbacade8e
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-12-09 00:38:31 +0700

    hoàn thành game thứ 2

---
+++ Doodle Jump/Doodle Jump/DoodleJump.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doodle_Jump
+{
+	public partial class DoodleJump : Form
+	{
+		Random r = new Random();
+		List<Point> plat= new List<Point>();
+		Bitmap backbuffer = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+		Image imagebackground = Image.FromFile("background.png");
+		Image imagePlat = Image.FromFile("platform.png");
+		Image imageDood = Image.FromFile("doodle.png");
+		Entities background;
+		List<Entities> Plat = new List<Entities>();
+		Entities Dood;
+		public DoodleJump()
+		{
+			InitializeComponent();
+			this.DoubleBuffered = true;
+			Init();
+			timer1.Enabled = true;
+		}
+		private void Init()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Point p = new Point(r.Next(0, this.Size.Width), r.Next(0, this.Size.Height));
+				plat.Add(p);
+				Entities tempplat = new Entities(imagePlat,
+					new Point(plat[plat.Count - 1].X, plat[plat.Count - 1].Y),
+					new Size(imagePlat.Size.Width, imagePlat.Size.Height), 0);
+				Plat.Add(tempplat);
+			}
+			background = new Entities(imagebackground, new Point(0, 0),
+				new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height), 0);
+			Dood = new Entities(imageDood, new Point(0, 0),
+				new Size(imageDood.Width, imageDood.Height), 0);
+		}
+		void Draw(Graphics g)
+		{
+			background.Draw(g);
+			for (int i = 0; i < Plat.Count; i++)
+			{
+				Plat[i].Draw(g);
+				Plat[i].Loc = plat[i];
+			}
+			Dood.Draw(g);
+		}
+		private void DoodleJump_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			Draw(g);
+		}
+
+		int x = 100, y = 100, h = 200;
+		int dx = 0, dy = 0;
+		bool goRight = false;
+		bool goLeft = false;
+		private void DoodleJump_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.A)
+			{
+				goLeft = false;
+			}
+			else if (e.KeyCode == Keys.D)
+			{
+				goRight = false;
+			}
+		}
+
+		private void DoodleJump_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode==Keys.A)
+			{
+				goLeft = true;
+			}
+			else if (e.KeyCode == Keys.D)
+			{
+				goRight = true;
+			}
+		}
+
+		private void MoveDood()
+		{	
+			dy += 2;
+			y += dy;
+			if (y > this.Height - Dood.Size.Height)
+			{
+				dy -= 20;
+			}
+			if(goLeft==true)
+			{
+				x -= 10;
+			}
+			if (goRight == true)
+			{
+				x += 10;
+			}
+			//Move Background
+			if (y < h)
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					int X = plat[i].X, Y;
+					y = h;
+					Y = plat[i].Y - dy;
+
+					if (plat[i].Y > this.Height)
+					{
+						Y = 0;
+						X = r.Next(0, this.Width);
+					}
+
+					plat[i] = new Point(X, Y);
+				}
+			}	
+			// Nhảy lên các thanh gạch
+			for (int i = 0; i < 10; i++)
+				if ((x + 50 > plat[i].X) && (x + 20 < plat[i].X + 68)
+				 && (y + 70 > plat[i].Y) && (y + 70 < plat[i].Y + 14) && (dy > 0)) dy = -30;
+
+			Dood.Loc = new Point(x, y);
+			this.Invalidate();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			MoveDood();
+		}
+	}
+}
---
+++ Doodle Jump/Doodle Jump/Entities.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+namespace Doodle_Jump
+{
+    enum Direction { Up, Down, Left, Right };
+    class Entities
+    {
+        protected Image image;
+        protected Point loc;
+        protected Size size;
+        protected int v;
+        public Entities(Image img, Point loc, Size size, int v)
+        {
+            this.image = img;
+            this.loc = loc;
+            this.size = size;
+            this.v = v;
+        }
+        public Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+        public Image Image { get => image; set => image = value; }
+        public Point Loc { get => loc; set => loc = value; }
+        public Size Size { get => size; set => size = value; }
+        public int V { get => v; set => v = value; }
+        public Rectangle Rect { get => new Rectangle(loc, size); }
+        virtual public void Draw(Graphics g)
+        {
+            g.DrawImage(ResizeImage(this.image, size.Width, size.Height),
+                new Rectangle(this.loc, this.size),
+                new Rectangle(0, 0, this.size.Width, this.size.Height),
+                GraphicsUnit.Pixel);
+        }
+        virtual public void Move(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.Down:
+                    loc.Y += v;
+                    break;
+                case Direction.Up:
+                    loc.Y -= v;
+                    break;
+                case Direction.Left:
+                    loc.X -= v;
+                    break;
+                case Direction.Right:
+                    loc.X += v;
+                    break;
+            }
+        }
+    }
+}
